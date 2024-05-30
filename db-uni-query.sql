6a88c57f6e964d2612b75dfa6f9907b17024560e
SimoneBonvecchio84 <simonebonvecchio@gmail.com>
2024-05-30 18:15:21 +0200

    Feat: query-join 2

--- db-uni-query.sql
+++ db-uni-query.sql
@@ -1,16 +1,19 @@
 1. Selezionare tutti gli studenti nati nel 1990
+
 SELECT * 
 FROM `students` 
 WHERE  YEAR(`date_of_birth`) = 1990 
 
 /*-------------------------------------------------------*/
 2. Selezionare tutti i corsi che valgono più di 10 crediti
+
 SELECT * 
 FROM `courses` 
 WHERE `cfu` > 10
 
 /*-------------------------------------------------------*/
 3. Selezionare tutti gli studenti che hanno più di 30 anni 
+
 SELECT * 
 FROM `students` 
 WHERE `date_of_birth` <= DATE_SUB(CURRENT_DATE(),INTERVAL 30 YEAR)
@@ -18,6 +21,7 @@ WHERE `date_of_birth` <= DATE_SUB(CURRENT_DATE(),INTERVAL 30 YEAR)
 /*-------------------------------------------------------*/
 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di
    laurea (286)
+
 SELECT * 
 FROM `courses` 
 WHERE `year` = 1 AND `period` = "I semestre"
@@ -25,6 +29,7 @@ WHERE `year` = 1 AND `period` = "I semestre"
 /*-------------------------------------------------------*/   
 5. Selezionare tutti gli appelli d esame che avvengono nel pomeriggio (dopo le 14) del
    20/06/2020 (21)  
+
 SELECT * 
 FROM `exams` 
 WHERE hour >= "14:00" 
@@ -32,17 +37,20 @@ AND `date` = "2020-06-20"
 
 /*-------------------------------------------------------*/     
 6. Selezionare tutti i corsi di laurea magistrale (38) 
+
 SELECT * 
 FROM `degrees` 
 WHERE `level` = "magistrale"  
 
 /*-------------------------------------------------------*/   
 7. Daquanti dipartimenti è composta l università? (12)
+
 SELECT COUNT(`id`) AS `departments_number`
 FROM `departments` 
 
 /*-------------------------------------------------------*/
 8. Quanti sono gli insegnanti che non hanno un numero di telefono?
+
 SELECT COUNT(`id`)
 FROM `teachers` 
 WHERE `phone` IS NULL
@@ -50,17 +58,20 @@ WHERE `phone` IS NULL
 /*-------------------------------------------------------*/
 9. Inserire nella tabella degli studenti un nuovo record con i propri dati (per il campo
    degree_id, inserire un valore casuale)
+
 INSERT INTO `students` ( `degree_id`, `name`,  `surname`, `date_of_birth`, `fiscal_code`, `enrolment_date`, `registration_number`, `email`)
 VALUES ("75", "Pippo", "Verdi", "1979-1-1", "AEGAYZ78P14C763V", "2020-1-1", "123456789", "pippo@verdi.com")   
 
 /*-------------------------------------------------------*/
 10. Cambiare il numero dell ufficio del professor Pietro Rizzo in 126
+
 UPDATE `teachers`
 SET `office_number` = 126
 WHERE `name` = "Pietro" AND `surname` = "Rizzo"
 
 /*-------------------------------------------------------*/
 11. Eliminare dalla tabella studenti il record creato precedentemente al punto 9
+
 DELETE FROM `students`
 WHERE `id` = 5001
 
@@ -75,6 +86,7 @@ LIMIT  5
 /*QUERY - GROUP*/
 
 1. Contare quanti iscritti ci sono stati ogni anno
+
 SELECT YEAR(`students`. `enrolment_date`) , COUNT(*)
 FROM `students`
 GROUP BY `students`.`enrolment_date`
@@ -82,6 +94,7 @@ GROUP BY `students`.`enrolment_date`
 /*-------------------------------------------------------*/
 
 2. Contare gli insegnanti che hanno l ufficio nello stesso edificio
+
 SELECT `teachers`.`office_address`, COUNT(*)
 FROM `teachers` 
 GROUP BY `teachers`.`office_address`
@@ -89,6 +102,7 @@ GROUP BY `teachers`.`office_address`
 /*-------------------------------------------------------*/
 
 3. Calcola La media dei voti di ogni appello d esame
+
 SELECT `exam_student`.`exam_id`, AVG(`exam_student`.`vote`) AS`media_voto`
 FROM `exam_student`
 GROUP BY `exam_student`.`exam_id`
@@ -112,4 +126,12 @@ ON `students`.`degree_id` = `degrees`.`id`
 WHERE `degrees`.`name` = "Corso di Laurea in Economia"
 
 /*-------------------------------------------------------*/
+
+2. Selezionare tutti i corsi di Laurea Magistrale del dipartimento di Neuroscienze
+
+SELECT * 
+FROM `degrees` 
+INNER JOIN `departments`
+ON `degrees`.`department_id` = `departments`.`id`
+WHERE `degrees`.`level` = "magistrale"
 
