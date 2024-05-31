9e3208df2b9408347a0c117cd390e443e74d0c60
SimoneBonvecchio84 <simonebonvecchio@gmail.com>
2024-05-31 10:02:05 +0200

    Fix: query-join 4

--- db-uni-query.sql
+++ db-uni-query.sql
@@ -158,13 +158,13 @@ AND `surname` = "Amato"
 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono
    iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
 
-SELECT *
+SELECT `students`.`name`, `students`.`surname`, `departments`.`name`
 FROM `students` 
 INNER JOIN `degrees`
 ON `students`.`degree_id` = `degrees`.`id`
-INNER JOIN `courses`
-ON `courses`.`degree_id` = `degrees`.`id`
-ORDER BY `students`.`surname` AND  `students`.`name` ASC   
+INNER JOIN `departments`
+ON `degrees`.`department_id` = `departments`.`id`
+ORDER BY `students`.`surname`, `students`.`name` ASC   
 
 /*-------------------------------------------------------*/
 
